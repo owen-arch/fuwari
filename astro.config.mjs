341da8d2fc74a1ce51fc128593c6cedf1f6318f2
owen-arch <00owen.po@gmail.com>
2025-08-03 17:08:54 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://owen-arch.github.io/",
+	base: "/fuwari/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
